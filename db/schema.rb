bf48b8135f8e03867bdde953286b701b5b5e7924
Foo Bar <crowley.13@gmx.de>
2015-05-11 15:04:35 +0200

    commented

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427101631) do
+ActiveRecord::Schema.define(version: 20150427101631) do#Stempel in schema_migrations; Nr.
+        #des zuletzt ausgefuehrten Migration-Scripts
 
   create_table "bookmarks", force: true do |t|
     t.string   "title"
